8a43467cd335c93c5f4b9180e0230685ab9d4903
Sunbreak <sunbreak.wang@gmail.com>
2020-12-06 17:10:29 +0800

    Initial commit
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ QuickPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickPad
+//
+//  Created by panyingying on 2020/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickPad/QuickPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPadApp.swift
+//  QuickPad
+//
+//  Created by panyingying on 2020/12/6.
+//
+
+import SwiftUI
+
+@main
+struct QuickPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
